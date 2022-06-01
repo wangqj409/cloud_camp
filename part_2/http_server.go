bc2d43bc3a2c7d2cd70ec05dd2c3476690bbc305
wangqj409 <wangqj409@gmail.com>
2022-06-02 07:11:30 +0800

    feat: add route healthz

--- part_2/http_server.go
+++ part_2/http_server.go
@@ -1,6 +1,7 @@
 package part_2
 
 import (
+    "fmt"
     "io"
     "net/http"
 )
@@ -11,15 +12,18 @@ var (
 
 func showHeaders(w http.ResponseWriter, r *http.Request) {
     for k := range r.Header {
-        io.WriteString(w, k)
-
+        io.WriteString(w, fmt.Sprintf("%s %v\n", k, r.Header.Get(k)))
     }
+}
+func healthz(w http.ResponseWriter, r *http.Request) {
+    io.WriteString(w, "200")
 }
 
 func NewServer() {
     serv = http.NewServeMux()
 
     serv.HandleFunc("/headers", showHeaders)
+    serv.HandleFunc("/healthz", healthz)
 
     http.ListenAndServe(":9002", serv)
 }
