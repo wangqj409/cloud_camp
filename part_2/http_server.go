9e3b762f9e032f1e9c505b848dcc9200958c88a0
wangqj409 <wangqj409@gmail.com>
2022-06-04 16:03:25 +0800

    opt: merge file

--- part_2/http_server.go
+++ part_2/http_server.go
@@ -1,7 +1,11 @@
 package part_2
 
 import (
+    "fmt"
+    "io"
     "net/http"
+    "os"
+    "time"
 )
 
 var (
@@ -14,6 +18,35 @@ var (
 - Server 端记录访问日志包括客户端 IP，HTTP 返回码，输出到 server 端的标准输出
 - 当访问 localhost/healthz 时，应返回 200
 */
+
+func responseHeaders(w http.ResponseWriter, r *http.Request) {
+    for k := range r.Header {
+        val := r.Header.Get(k)
+
+        // w.Header().Set(k, val)
+        // 🤔️？？？？  Set 字段不一定能被设置上，Add 很稳定
+        w.Header().Add(k, val)
+        io.WriteString(w, fmt.Sprintf("Got Header:%s value:%v\n", k, val))
+    }
+
+    fmt.Fprintf(os.Stdout, "%s %s %s %d\n", time.Now().Format(time.RFC3339), r.URL.String(), r.RemoteAddr, http.StatusOK)
+}
+
+func healthz(w http.ResponseWriter, r *http.Request) {
+    io.WriteString(w, "200")
+}
+
+func version(w http.ResponseWriter, r *http.Request) {
+
+    // k := r.PostFormValue("key")
+    // w.Write([]byte(os.Getenv(k)))
+
+    version := os.Getenv("VERSION")
+    w.Write([]byte(version))
+
+    w.Header().Add("VERSION", version)
+
+}
 
 
 func NewServer() {
